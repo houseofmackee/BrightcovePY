9edc3766d623c029ecf85cdcfc9f3f68b811e544
MacKenzie Glanzer <mglanzer@gmail.com>
2020-05-11 23:30:29 +0100

    added support for multiple accounts

--- enableIPRPlayers.py
+++ enableIPRPlayers.py
@@ -10,13 +10,18 @@ account_id, client_id, client_secret, _ = GetAccountInfo()
 # create a Player Management API instance
 pms = PlayerManagement( OAuth(account_id=account_id,client_id=client_id, client_secret=client_secret) )
 
-# get the items in the list of players
-playerList = pms.GetListOfPlayers(accountID=account_id).json()['items']
-
 # define the default JSON Body
 jsonBody = '{ "video_cloud": { "base_url": "https://edge-elb.api.brightcove.com/playback/v1/" } }'
 
-# patch every player in the account using the above JSON body
-for player in playerList:
-	playerID = player.get('id')
-	print('Patching player ID '+playerID+': '+str(pms.UpdatePlayerConfiguration(accountID=account_id, playerID=playerID, jsonBody=jsonBody).status_code))
+# list of accounts that need patching
+accountList = [ account_id ]
+
+for account in accountList:
+	print('Processing players in account ID '+str(account)+':')
+	# get the items in the list of players
+	playerList = pms.GetListOfPlayers(accountID=account).json()['items']
+
+	# patch every player in the account using the above JSON body
+	for player in playerList:
+		playerID = player.get('id')
+		print('Patching player ID '+playerID+': '+str(pms.UpdatePlayerConfiguration(accountID=account, playerID=playerID, jsonBody=jsonBody).status_code))
