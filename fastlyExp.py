0549b5e53256923c8650b6f61e0da5fa179765e8
MacKenzie Glanzer <mglanzer@gmail.com>
2020-05-27 22:17:20 +0100

    added example for how to decode the TTL for a fastly token

---
+++ fastlyExp.py
@@ -0,0 +1,10 @@
+import sys
+import base64
+import time
+
+if(len(sys.argv)>1):
+	token = str(base64.b64decode((sys.argv[1]).replace('%3D', '='))).replace('b\'','')
+	token = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int('0x'+token[:token.find('_')],0)))
+	print('Token expires: ' + token )
+else:
+	print('Missing token.')
