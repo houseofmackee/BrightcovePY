ab2ed6dede3b37e12a5f6b5d0fddd5412d07c9ea
MacKenzie Glanzer <mglanzer@gmail.com>
2020-06-19 22:53:37 +0100

    added sample to calculate average video duration in account

---
+++ findAverageDuration.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import mackee
+
+numberVideos = 0
+totalDuration = 0
+
+def convertMillis(millis):
+	millis = int(millis)
+	seconds=(millis/1000)%60
+	seconds = int(seconds)
+	minutes=(millis/(1000*60))%60
+	minutes = int(minutes)
+	hours=int((millis/(1000*60*60))%24)
+	return f'{hours:02}:{minutes:02}:{seconds:02}'
+
+#===========================================
+# callback add up video durations
+#===========================================
+def findAverageDuration(video):
+	global numberVideos
+	global totalDuration
+
+	duration = video.get('duration')
+	if(duration):
+		numberVideos += 1
+		totalDuration += duration
+
+#===========================================
+# only run code if it's not imported
+#===========================================
+if __name__ == '__main__':
+	mackee.main(findAverageDuration)
+	if(numberVideos>0):
+		print(f'Average duration for {numberVideos} videos is {convertMillis(totalDuration/numberVideos)}')
