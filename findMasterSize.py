c776b981f07082ae0269d489cb58a876505ea34f
MacKenzie Glanzer <mglanzer@gmail.com>
2020-09-12 14:13:32 +0100

    removed unused code

--- findMasterSize.py
+++ findMasterSize.py
@@ -6,7 +6,7 @@ import mackee
 #===========================================
 def getMasterStorage(video):
 	masterSize = 0
-	
+
 	if(video.get('has_digital_master')):
 		shared = video.get('sharing')
 		if(shared and shared.get('by_external_acct')):
@@ -18,25 +18,10 @@ def getMasterStorage(video):
 	return masterSize
 
 #===========================================
-# function to get size of all renditions
-#===========================================
-def getRenditionSizes(video):
-	renSize = 0
-
-	response = mackee.cms.GetDynamicRenditions(videoID=video.get('id'))
-	if(response.status_code in mackee.cms.success_responses):
-		renditions = response.json()
-		for rendition in renditions:
-			renSize += rendition.get('size')
-
-	return renSize
-
-#===========================================
 # callback to delete digital masters
 #===========================================
 def findStorageSize(video):
-	totalSize = getMasterStorage(video) # + getRenditionSizes(video)
-	print(str(video.get('id'))+', '+str(totalSize))
+	print(str(video.get('id'))+', '+str(getMasterStorage(video)))
 
 #===========================================
 # only run code if it's not imported
