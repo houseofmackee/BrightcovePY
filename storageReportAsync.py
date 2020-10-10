5b5e2e0389b2181420393393c5ff58196be0fa03
MacKenzie Glanzer <mglanzer@gmail.com>
2020-10-10 10:58:45 +0100

    added CSV output using -o option and cleaned up multi-threading code a bit

--- storageReportAsync.py
+++ storageReportAsync.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 import mackee
+import csv
+import time
 from threading import Thread
 from threading import Lock
 
 videosProcessed = 0
 counter_lock = Lock()
+data_lock = Lock()
+
+row_list = [ ['video_id','master_size','renditions_size'] ]
 
 def showProgress(progress):
 	mackee.sys.stderr.write(f'\r{progress} processed...\r')
@@ -56,20 +61,22 @@ def findStorageSize(video):
 		thread = Thread(target=functions[i], args=[video, results, i])
 		thread.start()
 		threads.append(thread)
+
 	# We now pause execution on the main thread by 'joining' all of our started threads.
 	# This ensures that each has finished processing the functions
 	for thread in threads:
 		thread.join()
 
-	masterSize = results[0]
-	renditionSize = results[1]
-	videoId = video.get('id') 
+	# add a new row to the CSV data
+	row = [ video.get('id'), results[0], results[1] ]
+	with data_lock:
+		row_list.append(row)
 
-	print(f'{videoId}, {masterSize}, {renditionSize}')
+	# increase processed videos counter
+	with counter_lock:
+		videosProcessed += 1
 
-	counter_lock.acquire()
-	videosProcessed += 1
-	counter_lock.release()
+	# display counter every 100 videos
 	if(videosProcessed%100==0):
 		showProgress(videosProcessed)
 
@@ -77,11 +84,21 @@ def findStorageSize(video):
 # only run code if it's not imported
 #===========================================
 if __name__ == '__main__':
-	import time
-	print('video_id, master_size, renditions_size')
 	s = time.perf_counter()
 	mackee.main(findStorageSize)
 	showProgress(videosProcessed)
+
+	#write list to file
+	try:
+		with open('report.csv' if not mackee.args.o else mackee.args.o, 'w', newline='', encoding='utf-8') as file:
+			try:
+				writer = csv.writer(file, quoting=csv.QUOTE_ALL, delimiter=',')
+				writer.writerows(row_list)
+			except Exception as e:
+				mackee.eprint(f'\nError writing CSV data to file: {e}')
+	except Exception as e:
+		mackee.eprint(f'\nError creating outputfile: {e}')
+
 	elapsed = time.perf_counter() - s
 	mackee.eprint(f"\n{__file__} executed in {elapsed:0.2f} seconds.")	
 
