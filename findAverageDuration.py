0990f36110085fdd8d85983701900ac29072d2af
MacKenzie Glanzer <mglanzer@gmail.com>
2020-11-20 13:44:20 +0000

    made thread safe and using new duration formatter class

--- findAverageDuration.py
+++ findAverageDuration.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python3
-import mackee
+import sys
+from mackee import main, TimeString as ts
+from threading import Lock
 
-numberVideos = 0
-totalDuration = 0
+num_videos = 0
+total_duration = 0
+data_lock = Lock()
+
+def showProgress(progress: int) -> None:
+	sys.stderr.write(f'\r{progress} processed...\r')
+	sys.stderr.flush()
 
 #===========================================
 # callback to add up video durations
 #===========================================
 def findAverageDuration(video):
-	global numberVideos
-	global totalDuration
+	global num_videos
+	global total_duration
 
 	duration = video.get('duration')
 	if(duration):
-		numberVideos += 1
-		totalDuration += (duration/1000)
+		with data_lock:
+			num_videos += 1
+			total_duration += (duration/1000)
+
+		# display counter every 100 videos
+		if(num_videos%100==0):
+			showProgress(num_videos)
 
 #===========================================
 # only run code if it's not imported
 #===========================================
 if __name__ == '__main__':
-	mackee.main(findAverageDuration)
-	if(numberVideos>0):
-		print(f'Average duration for {numberVideos} videos is {mackee.ConvertSeconds(totalDuration/numberVideos)}')
+	main(findAverageDuration)
+	showProgress(num_videos)
+	if(num_videos>0):
+		print(f'Average duration for {num_videos} videos with duration information is {ts.from_seconds(total_duration/num_videos)} (HH:MM:SS).')
