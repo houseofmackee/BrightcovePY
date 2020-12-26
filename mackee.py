66a9380a48846160fae19df1901fd8f0e2b78840
MacKenzie Glanzer <mglanzer@gmail.com>
2020-12-26 20:56:02 +0000

    updated type hints, doc strings and moved args def and parsing into get_args()

--- mackee.py
+++ mackee.py
@@ -7,12 +7,13 @@ import logging
 from os import path
 from json import JSONDecodeError
 from queue import Queue, Empty
-from typing import Callable, Dict, Any
+from typing import cast, Callable, Dict, Any
 from threading import Thread
 from dataclasses import dataclass
 from xlrd import XLRDError
 from pandas.errors import ParserError
 from requests.exceptions import RequestException
+from requests.models import Response
 import requests # pip3 install requests
 
 from brightcove.OAuth import OAuth
@@ -31,8 +32,9 @@ requests.urllib3.disable_warnings() #type: ignore
 # returns a DI instance
 #===========================================
 @static_vars(di=None)
-def get_di(oauth:OAuth=None, profile:str='', priority:str='normal') -> DynamicIngest:
-	"""Returns an existing DynamicIngest instance. Creates one if information is provided.
+def get_di(oauth: OAuth=None, profile: str='', priority: str='normal') -> DynamicIngest:
+	"""
+	Returns an existing DynamicIngest instance. Creates one if information is provided.
 
 	Args:
 		oauth (OAuth, optional): OAuth instance to use. Defaults to None.
@@ -42,7 +44,6 @@ def get_di(oauth:OAuth=None, profile:str='', priority:str='normal') -> DynamicIn
 	Returns:
 		DynamicIngest: DynamicIngest instance. None if none was created yet.
 	"""
-
 	if not get_di.di and oauth:
 		get_di.di = DynamicIngest(oauth=oauth, ingest_profile=profile, priority_queue=priority)
 		mac_logger.info('Obtained DI instance')
@@ -50,8 +51,9 @@ def get_di(oauth:OAuth=None, profile:str='', priority:str='normal') -> DynamicIn
 	return get_di.di
 
 @static_vars(cms=None)
-def get_cms(oauth:OAuth=None, query:str='') -> CMS:
-	"""Returns an existing CMS instance. Creates one if information is provided.
+def get_cms(oauth: OAuth=None, query: str='') -> CMS:
+	"""
+	Returns an existing CMS instance. Creates one if information is provided.
 
 	Args:
 		oauth (OAuth, optional): OAuth instance to use. Defaults to None.
@@ -67,7 +69,10 @@ def get_cms(oauth:OAuth=None, query:str='') -> CMS:
 	return get_cms.cms
 
 @static_vars(oauth=None)
-def get_oauth(account_id:str='', client_id:str='', client_secret:str='') -> OAuth:
+def get_oauth(account_id: str='', client_id: str='', client_secret: str='') -> OAuth:
+	"""
+	Returns an OAuth instance. Creates one if it doesn't exist yet.
+	"""
 	if not get_oauth.oauth:
 		get_oauth.oauth = OAuth(account_id, client_id, client_secret)
 		mac_logger.info('Obtained OAuth instance')
@@ -75,15 +80,39 @@ def get_oauth(account_id:str='', client_id:str='', client_secret:str='') -> OAut
 	return get_oauth.oauth
 
 @static_vars(args=None)
-def get_args(parser=None):
-	if not get_args.args and parser:
+def get_args():
+	"""
+	Returns the command line arguments. Parses them if not parsed yet.
+	"""
+	if not get_args.args:
+		# init the argument parsing
+		parser = argparse.ArgumentParser(prog=sys.argv[0])
+		parser.add_argument('-i', type=str, help='Name and path of account config information file')
+		parser.add_argument('-q', type=str, help='CMS API search query')
+		parser.add_argument('-t', type=str, help='Target account ID')
+		parser.add_argument('-v', type=str, help='Specific video ID to process')
+		parser.add_argument('-o', type=str, help='Output filename')
+		parser.add_argument('-x', type=str, help='XLS/CSV input filename')
+		parser.add_argument('-a', type=int, const=10, nargs='?', help='Async processing of videos')
+		parser.add_argument('-d', action='store_true', default=False, help='Show debug info messages')
+
 		get_args.args = parser.parse_args()
+
+		if get_args.args.d:
+			logging.basicConfig(level=logging.INFO, format='[%(levelname)s:%(lineno)d]: %(message)s')
+			mac_logger.info('Logging at INFO level enabled')
+		else:
+			logging.basicConfig(level=logging.CRITICAL, format='[%(levelname)s:%(lineno)d]: %(message)s')
+
 		mac_logger.info('Obtained arguments')
 
 	return get_args.args
 
 @static_vars(session=None)
 def get_session():
+	"""
+	Returns a requests session. Creates one if it doesn't exist yet.
+	"""
 	if not get_session.session:
 		get_session.session = requests.Session()
 		mac_logger.info('Obtained Requests Session')
@@ -94,21 +123,24 @@ def get_session():
 # default processing function
 #===========================================
 @static_vars(print_header=True)
-def list_videos(video:dict) -> None:
+def list_videos(video: dict) -> None:
+	"""
+	Default callback function. Prints out video ID and name.
+	"""
 	if list_videos.print_header:
 		print('video_id, title')
 		list_videos.print_header = False
 
 	print(f'{video.get("id")}, {video.get("name")}')
 
-def get_accounts(account_parameter:str) -> list:
+def get_accounts(account_parameter: str) -> list:
 	"""
-	Function to generate a list of account IDs from a CSV/XLS file, a comma separated command line option or just the default account ID from the config.
+	Function to generate a list of account IDs from a CSV/XLS file, a comma separated
+	command line option or just the default account ID from the config.
 
 	Returns:
 		list: [description]
 	"""
-
 	if path.isfile(account_parameter):
 		return videos_from_file(filename=account_parameter, column_name='account_id')
 
@@ -117,12 +149,11 @@ def get_accounts(account_parameter:str) -> list:
 
 	return [account_parameter]
 
-
 #===========================================
 # function to fill queue with all videos
 # from a Video Cloud account
 #===========================================
-def process_account(work_queue:Queue, account_id:str, cms_obj:CMS) -> None:
+def process_account(work_queue: Queue, account_id: str, cms_obj: CMS) -> None:
 	"""
 	Function to fill a Queue with a list of all video IDs in an account.
 
@@ -131,7 +162,6 @@ def process_account(work_queue:Queue, account_id:str, cms_obj:CMS) -> None:
 		cms_obj (CMS): CMS class instance
 		account_id (str): Video Cloud account ID
 	"""
-
 	# ok, let's process all videos
 	# get number of videos in account
 	try:
@@ -156,12 +186,12 @@ def process_account(work_queue:Queue, account_id:str, cms_obj:CMS) -> None:
 			response = cms_obj.GetVideos(account_id=account_id, page_size=page_size, page_offset=current_offset)
 			status = response.status_code
 		except RequestException:
-			response = None # type: ignore
+			response = cast(Response, None)
 			status = -1
 
 		# good result
 		if status in [200,202]:
-			json_data = response.json() # type: ignore
+			json_data = cast(dict, response.json())
 			# make sure we actually got some data
 			if len(json_data) > 0:
 				# let's put all videos in a queue
@@ -202,8 +232,9 @@ def process_account(work_queue:Queue, account_id:str, cms_obj:CMS) -> None:
 #===========================================
 # function to process a single video
 #===========================================
-def process_single_video_id(account_id:str, video_id:str, cms_obj:CMS, process_callback:Callable[[Dict[Any, Any]], None]) -> bool:
-	"""Function to process a single video using a provided callback function
+def process_single_video_id(account_id: str, video_id: str, cms_obj: CMS, process_callback: Callable[[Dict[Any, Any]], None]) -> bool:
+	"""
+	Function to process a single video using a provided callback function
 
 	Args:
 		account_id (str): the account ID
@@ -214,7 +245,6 @@ def process_single_video_id(account_id:str, video_id:str, cms_obj:CMS, process_c
 	Returns:
 		bool: True if there was no error, False otherwise
 	"""
-
 	response = None
 	try:
 		response = cms_obj.GetVideo(account_id=account_id, video_id=video_id)
@@ -238,24 +268,39 @@ def process_single_video_id(account_id:str, video_id:str, cms_obj:CMS, process_c
 
 	return True
 
-# worker class for multithreading
 class Worker(Thread):
+	"""
+	Worker class for multithreading using queues.
+	"""
 
 	@dataclass
 	class WorkerData:
+		"""
+		Dataclass to define processing parameters. Not used yet.
+		"""
 		queue: Queue
 		account_id: str
 		callback: Callable
 		cms_instance: CMS
 
-	def __init__(self, q, cms_obj, account_id, process_callback, *args, **kwargs):
-		self.queue = q
+	def __init__(self, queue:Queue, cms_obj: CMS, account_id: str, process_callback: Callable, *args, **kwargs):
+		"""
+		Args:
+			queue (Queue): Queue to process.
+			cms_obj (CMS): CMS instance to use.
+			account_id (str): Brightcove account ID.
+			process_callback (Callable): Callback function which processes the data.
+		"""
+		super().__init__(*args, **kwargs)
+		self.queue = queue
 		self.cms_obj = cms_obj
 		self.account_id = account_id
 		self.process_callback = process_callback
-		super().__init__(*args, **kwargs)
 
 	def run(self):
+		"""
+		Processes the queue.
+		"""
 		keep_working = True
 		while keep_working:
 			try:
@@ -275,13 +320,23 @@ class Worker(Thread):
 										video_id=work,
 										cms_obj=self.cms_obj,
 										process_callback=self.process_callback)
-
 			self.queue.task_done()
 
 #===========================================
 # this is the main loop to process videos
 #===========================================
-def process_input(account_info_file:str=None, process_callback:Callable[[Dict[Any, Any]], None]=list_videos, video_id:str=None) -> bool:
+def process_input(account_info_file: str='', process_callback: Callable=list_videos, video_id: str='') -> bool:
+	"""
+	Function to process whatever was passed to the script.
+
+	Args:
+		account_info_file (str, optional): Account info JSON file to use. Defaults to ''.
+		process_callback (Callable, optional): Name of function to process the data. Defaults to list_videos.
+		video_id (str, optional): Video ID to process. Defaults to ''.
+
+	Returns:
+		bool: True if video processed successfully, False otehrwise.
+	"""
 	# get the account info and credentials
 	try:
 		account_id, client_id, client_secret, opts = load_account_info(account_info_file)
@@ -305,7 +360,7 @@ def process_input(account_info_file:str=None, process_callback:Callable[[Dict[An
 
 	# if async is enabled use more than one thread
 	max_threads = get_args().a or 1
-	mac_logger.info(f'Using {max_threads} thread(s) for processing')
+	mac_logger.info('Using %d thread(s) for processing', max_threads)
 
 	#=========================================================
 	#=========================================================
@@ -338,13 +393,13 @@ def process_input(account_info_file:str=None, process_callback:Callable[[Dict[An
 		num_videos = len(video_list)
 		eprint(f'Found {num_videos} videos in options file. Processing them now.')
 		# let's put all video IDs in a queue
-		for video_id in video_list:
-			work_queue.put_nowait(video_id)
+		for video_id_ in video_list:
+			work_queue.put_nowait(video_id_)
 		# starting worker threads on queue processing
 		num_threads = min(max_threads, num_videos)
 		for _ in range(num_threads):
 			work_queue.put_nowait("EXIT")
-			Worker(	q=work_queue,
+			Worker(	queue=work_queue,
 					cms_obj=get_cms(),
 					account_id=account_id,
 					process_callback=process_callback).start()
@@ -368,7 +423,7 @@ def process_input(account_info_file:str=None, process_callback:Callable[[Dict[An
 
 		# starting worker threads on queue processing
 		for _ in range(max_threads):
-			Worker(q=work_queue, cms_obj=get_cms(), account_id=account_id, process_callback=process_callback).start()
+			Worker(queue=work_queue, cms_obj=get_cms(), account_id=account_id, process_callback=process_callback).start()
 
 		# first wait for the queue filling thread to finish
 		account_page_thread.join()
@@ -386,26 +441,16 @@ def process_input(account_info_file:str=None, process_callback:Callable[[Dict[An
 #===========================================
 # parse args and do the thing
 #===========================================
-def main(process_func:Callable[[Dict[Any, Any]], None]) -> None:
-	# init the argument parsing
-	parser = argparse.ArgumentParser(prog=sys.argv[0])
-	parser.add_argument('-i', type=str, help='Name and path of account config information file')
-	parser.add_argument('-q', type=str, help='CMS API search query')
-	parser.add_argument('-t', type=str, help='Target account ID')
-	parser.add_argument('-v', type=str, help='Specific video ID to process')
-	parser.add_argument('-o', type=str, help='Output filename')
-	parser.add_argument('-x', type=str, help='XLS/CSV input filename')
-	parser.add_argument('-a', type=int, const=10, nargs='?', help='Async processing of videos')
-	parser.add_argument('-d', action='store_true', default=False, help='Show debug info messages')
+def main(process_func: Callable) -> None:
+	"""
+	This will parse command line arguments, get account credentials and then processess
+	whatever was passed along.
 
+	Args:
+		process_func (Callable): Name of function to process the data.
+	"""
 	# parse the args
-	get_args(parser)
-
-	if get_args().d:
-		logging.basicConfig(level=logging.INFO, format='[%(levelname)s:%(lineno)d]: %(message)s')
-		mac_logger.info('Logging at INFO level enabled')
-	else:
-		logging.basicConfig(level=logging.CRITICAL, format='[%(levelname)s:%(lineno)d]: %(message)s')
+	get_args()
 
 	# go through the library and do stuff
 	process_input(account_info_file=get_args().i, process_callback=process_func, video_id=get_args().v)
