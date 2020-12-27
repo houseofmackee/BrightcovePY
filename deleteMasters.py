0e0392f347f44ab8e0feff385f82250f243e79d1
MacKenzie Glanzer <mglanzer@gmail.com>
2020-12-27 12:40:28 +0000

    updated CMS API call name to reflect changes in CMS wrapper

--- deleteMasters.py
+++ deleteMasters.py
@@ -13,7 +13,7 @@ def delete_masters(video: dict):
 		if shared and shared.get('by_external_acct'):
 			return
 		video_id = video.get('id')
-		print(f'Deleting master for video ID {video_id}: {get_cms().DeleteMaster(video_id=video_id).status_code}')
+		print(f'Deleting master for video ID {video_id}: {get_cms().DeleteDigitalMaster(video_id=video_id).status_code}')
 
 #===========================================
 # only run code if it's not imported
