468120de7109702a1befea63af8a5a12033b60d2
MacKenzie Glanzer <mglanzer@gmail.com>
2021-03-03 15:21:06 +0000

    switched to using new performance timer from utils

--- createReport.py
+++ createReport.py
@@ -4,10 +4,13 @@ from csv import Error as CSVError
 from time import perf_counter
 from mackee import main, get_args
 from brightcove.utils import list_to_csv, eprint
-from brightcove.utils import SimpleProgressDisplay, TimeString
+from brightcove.utils import SimpleProgressDisplay, SimpleTimer
 from brightcove.utils import get_value, default_split
 
 # list of information to be added to the report (edit as needed)
+# use . to to get sub fields (like specific custom fields (e.g. 'custom_fields.my_field'))
+# use : to specify a default value in case the response is empty (e.g 'name:NoName')
+# use [] to specify a specific index
 row_list = [ ('account_id','id', 'name', 'state', 'reference_id', 'created_at', 'tags', 'created_by.type') ]
 
 # some globals
@@ -21,7 +24,6 @@ def create_report(video: dict) -> None:
 	Args:
 		video (dict): video object obtained from the CMS API.
 	"""
-
 	row = (get_value(video, *default_split(field, separator=':', maxsplits=1)) for field in row_list[0])
 	with data_lock:
 		row_list.append(row)
@@ -31,16 +33,12 @@ def create_report(video: dict) -> None:
 # only run code if it's not imported
 #===========================================
 if __name__ == '__main__':
-	# generate the report
-	s = perf_counter()
-	main(create_report)
-	show_progress(force_display=True)
-
-	# write report to CSV file
-	try:
-		list_to_csv(row_list, get_args().o)
-	except (OSError, CSVError) as e:
-		eprint(f'\n{e}')
-
-	elapsed = perf_counter() - s
-	eprint(f"\n{__file__} executed in {TimeString.from_seconds(elapsed)}.")
+    with SimpleTimer():
+        # generate the report
+        main(create_report)
+        show_progress(force_display=True)
+        # write report to CSV file
+        try:
+            list_to_csv(row_list, get_args().o)
+        except (OSError, CSVError) as e:
+            eprint(f'\n{e}')
