4d2b2cc4a24de8f365f68b07fe8ec5de972ea70b
MacKenzie Glanzer <mglanzer@gmail.com>
2021-07-20 12:35:50 +0100

    removed use of global variable

--- updateContentType.py
+++ updateContentType.py
@@ -2,10 +2,18 @@
 Example script to update content type for videos in an account
 """
 from mackee import main, get_oauth
+from brightcove.utils import static_vars
 from brightcove.Audience import Audience
 
-# global Audience API instance
-aapi : Audience = None
+# get Audience API instance
+@static_vars(aapi=None)
+def get_audience() -> Audience:
+    """
+    Returns an OAuth instance. Creates one if it doesn't exist yet.
+    """
+    if not get_audience.aapi:
+        get_audience.aapi = Audience(get_oauth())
+    return get_audience.aapi
 
 #===========================================
 # callback to set content type for videos
@@ -14,18 +22,12 @@ def update_content_type(video: dict):
     """
     Updates content type for a video.
     """
-    global aapi
-
-    # get an Audience API instance in case we don't have one yet
-    if not aapi:
-        aapi = Audience(get_oauth())
-
     # get video ID and specify content type
     video_id = video.get('id')
     content_type = 'my-type-here'
 
     # make API call
-    response = aapi.SetContentType(video_id=video_id, content_type=content_type)
+    response = get_audience().SetContentType(video_id=video_id, content_type=content_type)
 
     # check if we have a success response
     if response.status_code in [200,202]:
